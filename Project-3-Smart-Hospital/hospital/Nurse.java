c6c4e60add7a302294b55a76c49fa48c77fee187
Srinidhi-U <srinidhiuppula4@gmail.com>
2026-09-06 18:30:28 +0530

    Add nurse child class from PDF

---
+++ Project-3-Smart-Hospital/hospital/Nurse.java
@@ -0,0 +1,13 @@
+package hospital;
+
+public class Nurse extends Staff {
+    private String ward;
+
+    public void setWard(String ward) {
+        this.ward = ward;
+    }
+
+    public void displayNurseDetails() {
+        System.out.println("Ward: " + ward);
+    }
+}
