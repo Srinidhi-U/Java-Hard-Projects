983ef5efb3270d39f797eb245c6e025d39ea1dd4
Srinidhi-U <srinidhiuppula4@gmail.com>
2026-09-06 18:30:23 +0530

    Add doctor child class from PDF

---
+++ Project-3-Smart-Hospital/hospital/Doctor.java
@@ -0,0 +1,13 @@
+package hospital;
+
+public class Doctor extends Staff {
+    private String specialization;
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public void displayDoctorDetails() {
+        System.out.println("Specialization: " + specialization);
+    }
+}
