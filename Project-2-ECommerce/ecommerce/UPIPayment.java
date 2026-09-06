55d650c173986784230e00fc604afdcaf24f6288
Srinidhi-U <srinidhiuppula4@gmail.com>
2026-09-06 18:12:18 +0530

    Add UPI payment implementation

---
+++ Project-2-ECommerce/ecommerce/UPIPayment.java
@@ -0,0 +1,8 @@
+package ecommerce;
+
+public class UPIPayment implements Payment {
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid Rs." + amount + " using UPI");
+    }
+}
