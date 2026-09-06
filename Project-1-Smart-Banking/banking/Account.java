0bdd520545df56f1843b5f97e526b6a5121d1613
Srinidhi-U <srinidhiuppula4@gmail.com>
2026-09-06 18:28:13 +0530

    Update banking base class to match PDF

--- Project-1-Smart-Banking/banking/Account.java
+++ Project-1-Smart-Banking/banking/Account.java
@@ -1,47 +1,19 @@
 package banking;
 
 public class Account {
-    private String name;
-    private int accountNumber;
+    private String accountHolder;
     private double balance;
 
-    public Account(String name, int accountNumber, double balance) {
-        this.name = name;
-        this.accountNumber = accountNumber;
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+    public void setBalance(double balance) {
         this.balance = balance;
     }
 
-    public void deposit(double amount) {
-        if (amount > 0) {
-            balance += amount;
-            System.out.println("Deposited: " + amount);
-        }
-    }
-
-    public void withdraw(double amount) {
-        if (amount > 0 && amount <= balance) {
-            balance -= amount;
-            System.out.println("Withdrawn: " + amount);
-        } else {
-            System.out.println("Insufficient balance");
-        }
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void display() {
-        System.out.println("Name: " + name);
-        System.out.println("Account No: " + accountNumber);
+    public void displayAccountDetails() {
+        System.out.println("Account Holder: " + accountHolder);
         System.out.println("Balance: " + balance);
     }
 }
