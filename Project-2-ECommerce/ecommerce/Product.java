a9a7d1140149b27ed3bd717ec53d93ac9a891d73
Srinidhi-U <srinidhiuppula4@gmail.com>
2026-09-06 18:11:51 +0530

    Add e-commerce Product class

---
+++ Project-2-ECommerce/ecommerce/Product.java
@@ -0,0 +1,25 @@
+package ecommerce;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void display() {
+        System.out.println(id + " - " + name + " - Rs." + price);
+    }
+}
