c67d06019dc8fcb326c3e233da7e301ec9650e46
Srinidhi-U <srinidhiuppula4@gmail.com>
2026-09-06 18:30:19 +0530

    Add staff parent class from PDF

---
+++ Project-3-Smart-Hospital/hospital/Staff.java
@@ -0,0 +1,19 @@
+package hospital;
+
+public class Staff {
+    private String name;
+    private int staffId;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStaffId(int staffId) {
+        this.staffId = staffId;
+    }
+
+    public void displayStaffDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Staff ID: " + staffId);
+    }
+}
