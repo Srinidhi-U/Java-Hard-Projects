c06a23ebb311ba35856a9cf636ee2e43c6f7ea21
Srinidhi-U <srinidhiuppula4@gmail.com>
2026-09-15 10:39:42 +0530

    Add basic exception handling program

---
+++ ExceptionHandling/ExceptionDemo.java
@@ -0,0 +1,13 @@
+class ExceptionDemo {
+
+    public static void main(String[] args) {
+
+        try {
+            int a = 10 / 0;
+            System.out.println(a);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+    }
+}
